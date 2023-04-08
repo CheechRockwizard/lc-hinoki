899381e4ab7e3d93cf086c1723c693fedfa0c1e4
Dan Chillibyte <dan@chillibyte.co.uk>
2023-04-08 13:34:19 +0100

    fp

--- page-templates/blocks/featured.php
+++ page-templates/blocks/featured.php
@@ -1,6 +1,13 @@
+<?php
+$posts = get_field('post');
+?>
 <!-- featured post -->
 <section class="featured">
     <div class="container-xl">
-        FEATURED
+        <?php
+        foreach ($posts as $p) {
+            echo $p;
+        }
+?>
     </div>
 </section>
